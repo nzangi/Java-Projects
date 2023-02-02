a113a92f59849b1168de9db8177fde369744eb33
nzangi <nzangimuoki284@gmail.com>
2023-02-02 11:35:19 +0300

    Java Projects

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,32 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
 
+//class main
+import java.io.*;
+public class Main {
+//  main method/function
+    public static void main(String[] args) {
+//        initializing values
+//        int value
+        int number = 89;
+//        char value
+        char myChar = 'G';
+//        double
+        double myDouble = 4.343434;
+//        short
+        short myShort = 34;
+//        float
+        float myFloat = 234.775667776f;
+//        long
+        long myLong = 12123;
+//        byte
+        byte myByte = 5;
+
+        System.out.println("The integer is : " + number);
+        System.out.println("The char is : "+myChar);
+        System.out.println("The short is : " + myShort);
+        System.out.println("The double is : "+myDouble);
+        System.out.println("The long is : " + myLong);
+        System.out.println("The byte is : "+myByte);
+        System.out.println("The float is : "+myFloat);
 
     }
 }
